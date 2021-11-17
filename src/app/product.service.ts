5867b30587a80a6671b6e5212db9a82bc8ca21bd
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2021-11-17 01:04:00 +0100

    using interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-import {Http, Response} from '@angular/http';
-import { AlbumStorePage } from '../../e2e/app.po';
+// import {Http, Response} from '@angular/http';
+// import { AlbumStorePage } from '../../e2e/app.po';
+
+// import 'rxjs/add/operator/map';
+
+// @Injectable()
+// export class ProductService {
+
+//   private _albumUrl: string = '../assets/album.json';
+
+//   constructor(private _http: Http) {}
+
+//   getAlbum (id:number) {
+//     return this._http.get(this._albumUrl).map((response) => response.json());
+//   }
+
+// }
+
+// using observables
+
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 import 'rxjs/add/operator/map';
+
+import { Album } from './album';
+
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
-  private _albumUrl: string = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
-  constructor(private _http: Http) {}
+  constructor(private _http: Http) { }
 
-  getAlbum (id:number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
-
 }
---
+++ src/app/trac.ts
@@ -0,0 +1,7 @@
+export interface Trac {
+    trackNumber: number;
+    trackName: string;
+    trackLength: string;
+    trackPrice: number;
+  
+}
